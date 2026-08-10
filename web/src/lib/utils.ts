ae21afa24639d3eda518f99733b5a16c4bd480d0
Sandeep Dinesh <sandeep@buildwithfern.com>
2026-08-10 12:46:32 -0700

    Refactor formatViewHeading to return empty string
    Simplify formatViewHeading function to return an empty string if one date is provided.

--- web/src/lib/utils.ts
+++ web/src/lib/utils.ts
@@ -82,11 +82,7 @@ export function viewDatesFor(activeDate: string, mode: ScheduleViewMode): string
 }
 
 export function formatViewHeading(dates: string[]): string {
-  if (dates.length === 1) {
-    return '';
-  }
-
-  return formatWeekRange(dates);
+  return ''
 }
 
 export function formatShortDate(date: string): string {
